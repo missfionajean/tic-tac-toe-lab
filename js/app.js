b7fd485f96b04a8dab22f27d37d262ac4d1f9f21
missfionajean <missfionajean@gmail.com>
2024-10-28 11:08:14 -0400

    added basic board manipulation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 /*------------------------- Constants -------------------------*/
 
+const winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 
 /*--------------------- Variables (state) ---------------------*/
 
-let board = ['','','','','','','','','']
+let board = ["", "", "", "", "", "", "", "", ""];
 
 /* 
 Board Array Ref:
@@ -12,27 +22,53 @@ Board Array Ref:
 6 | 7 | 8
  */
 
-let turn = 'x' // alternates between 'x' and 'o'
-let winner = false // used to check if a win has been acheieved
-let tie = false // used for tie message when board full and no win
+let turn = "x"; // alternates between 'x' and 'o'
+let winner = false; // used to check if a win has been acheieved
+let tie = false; // used for tie message when board full and no win
 
 /*----------------- Cached Element References -----------------*/
 
-const squareEls = document.querySelector('.sqr')
-const messageEl = document.querySelector('#message')
+const squareEls = document.querySelector(".board");
+const messageEl = document.querySelector("#message");
 
 /*------------------------- Functions -------------------------*/
 
-const init = () => {
+const init = () => {};
 
-}
+const render = () => {};
 
-/*---------------------- Event Listeners ----------------------*/
+const updateBoard = (event) => {
+	if (
+		event.target.classList.contains("sqr") &&
+		event.target.innerText === ""
+	) {
+		event.target.innerText = turn;
+		if (turn === "x") {
+			turn = "o";
+		} else {
+			turn = "x";
+		}
+		updateMessage();
+	}
+};
 
-init()
+const updateMessage = () => {
+	if (turn === "x") {
+		messageEl.innerText = "It's X's turn!";
+	} else if (turn === "o") {
+		messageEl.innerText = "It's O's turn!";
+	} else {
+		messageEl.innerText = "Welcome to Tic-Tac-Toe!";
+	}
+};
 
-// render() [will add this in soon]
+/*---------------------- Listeners/Calls ----------------------*/
 
+init();
+
+squareEls.addEventListener("click", updateBoard);
+
+// render();
 
 /*----------------------- User Stories ------------------------*/
 
@@ -60,7 +96,7 @@ Provide a Reset Game button that will clear the contents of the board.
 
 //2) Store cached element references.
 
-//3) Upon loading, the game state should be initialized, and a function should 
+//3) Upon loading, the game state should be initialized, and a function should
 //   be called to render this game state.
 
 //4) The state of the game should be rendered to the user.
